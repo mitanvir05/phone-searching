f0ae9961be0b9f111a31d6d1e6d7fe92fe0605c4
mitanvir05 <tanvir.cse05@gmail.com>
2022-03-01 00:37:30 +0600

    first part done

--- js/app.js
+++ js/app.js
@@ -2,8 +2,26 @@ const allPhones = () => {
     const searchValue = document.getElementById("search-box").value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
-        .then(response => response.json())
-        .then(data => console.log(data));
+        .then((response) => response.json())
+        .then(data => showPhone(data.data));
+}
 
-    // console.log(url);
+const showPhone = (phones) => {
+    for(const data of phones){
+     const parent = document.getElementById("phone-container");
+     const div = document.createElement("div");
+     div.innerHTML = `<div class="card border p-5">
+                <div class="pro-pic p-2 ">
+                    <img class="w-25" src="${data.image}" alt="">
+                </div>
+                <h4>Name : ${data.phone_name}</h4>
+                <h3>Brand : ${data.brand}</h3>
+                <div class="button">
+                    <button class="btn btn-success">Details</button>
+                </div>
+            </div>`;
+        parent.appendChild(div);
+        console.log (data)
+    }
+    
 }
