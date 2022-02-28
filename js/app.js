42b48514285e6e1d20508fff414207be9a378c18
mitanvir05 <tanvir.cse05@gmail.com>
2022-03-01 01:32:27 +0600

    first part with grid

--- js/app.js
+++ js/app.js
@@ -7,21 +7,27 @@ const allPhones = () => {
 }
 
 const showPhone = (phones) => {
-    for(const data of phones){
-     const parent = document.getElementById("phone-container");
-     const div = document.createElement("div");
-     div.innerHTML = `<div class="card border p-5">
-                <div class="pro-pic p-2 ">
+    for (const data of phones) {
+        const parent = document.getElementById("phone-container");
+        const div = document.createElement("div");
+        div.innerHTML = `<div class="card border p-5 ">
+                <div class="pro-pic p-2">
                     <img class="w-25" src="${data.image}" alt="">
                 </div>
                 <h4>Name : ${data.phone_name}</h4>
                 <h3>Brand : ${data.brand}</h3>
                 <div class="button">
-                    <button class="btn btn-success">Details</button>
+                    <button onclick="details('${data.slug}')" class="btn btn-success">Details</button>
                 </div>
             </div>`;
         parent.appendChild(div);
-        console.log (data)
     }
-    
+
+}
+
+const details = (id) => {
+    const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+    fetch(url)
+    .then((response) => response.json())
+    .then (data => console.log(data));
 }
