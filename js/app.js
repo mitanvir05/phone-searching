21361333d5941b31e8d507d5b6ff7d34011e1fca
mitanvir05 <tanvir.cse05@gmail.com>
2022-03-01 22:56:40 +0600

    without showing 20 device

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 const allPhones = () => {
+    document.getElementById("phone-container").innerHTML = '';
     const searchValue = document.getElementById("search-box").value;
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchValue}`;
     fetch(url)
         .then((response) => response.json())
         .then(data => showPhone(data.data));
-}
+    document.getElementById("search-box").value = '';
+};
 
 const showPhone = (phones) => {
     for (const data of phones) {
@@ -14,8 +16,8 @@ const showPhone = (phones) => {
                 <div class="pro-pic p-2">
                     <img class="w-25" src="${data.image}" alt="">
                 </div>
-                <h4>Name : ${data.phone_name}</h4>
-                <h3>Brand : ${data.brand}</h3>
+                <h3>Name : ${data.phone_name}</h3>
+                <h4>Brand : ${data.brand}</h4>
                 <div class="button">
                     <button onclick="details('${data.slug}')" class="btn btn-success">Details</button>
                 </div>
@@ -23,11 +25,27 @@ const showPhone = (phones) => {
         parent.appendChild(div);
     }
 
-}
+};
 
 const details = (id) => {
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
     fetch(url)
-    .then((response) => response.json())
-    .then (data => console.log(data));
-}
+        .then((response) => response.json())
+        .then(data => setDetails(data.data));
+};
+const setDetails = (info) => {
+    console.log(info);
+    document.getElementById("details-container").innerHTML = `
+            <div class="card border p-5 ">
+                <div class="pro-pic p-2">
+                    <img class="w-25" src="${info.image}" alt="">
+                </div>
+                <h3>Name : ${info.name}</h3>
+                <h4>Brand : ${info.brand}</h4>
+                <h4> Release : ${info.releaseDate}</h4>
+                <h4> Chipset : ${info.mainFeatures.chipSet}</h4>
+                <h4> Dislay : ${info.mainFeatures.displaySize}</h4>
+                <h4> Sensor : ${info.mainFeatures.sensors}</h4>
+                
+            </div>`;
+};
